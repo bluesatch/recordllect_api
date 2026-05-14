d1e3cb99880fef068a606f3d3923e4ad4c5f2998
bluesatch <satchwerk@gmail.com>
2026-05-14 10:44:04 -0500

    fixed error in uploadRoutes.js

--- routes/api/uploadRoutes.js
+++ routes/api/uploadRoutes.js
@@ -15,7 +15,7 @@ router.post('/image', auth, uploadImage.single('image'), uploadController.upload
 router.post('/video', auth, uploadVideo.single('video'), uploadController.uploadVideo)
 
 // Avatar upload - for profile image 
-router.post('/avatar', auth, uploadAvatar.single('avatar')., uploadController.uploadAvatar)
+router.post('/avatar', auth, uploadAvatar.single('avatar'), uploadController.uploadAvatar)
 
 // Delete file from Cloudinary 
 router.delete('/', auth, uploadController.deleteFile)
