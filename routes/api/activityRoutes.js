c03702c86c0a1f62ea4949212c5e007b5c58d1d3
bluesatch <satchwerk@gmail.com>
2026-05-25 14:45:23 -0500

    Found auth

--- routes/api/activityRoutes.js
+++ routes/api/activityRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const { getActivityFeed } = require('../../controllers/activityController')
-const auth = require('../middleware/auth')
+const auth = require('../../middleware/auth')
 
 router.get('/feed', auth, getActivityFeed)
 
