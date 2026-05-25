ab41cba89aa5bd23c352b0808df035f2ced34287
bluesatch <satchwerk@gmail.com>
2026-05-25 14:42:16 -0500

    Fixing bug

--- routes/api/activityRoutes.js
+++ routes/api/activityRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { getActivityFeed } = require('../controllers/activityController')
+const { getActivityFeed } = require('../../controllers/activityController')
 const auth = require('../middleware/auth')
 
 router.get('/feed', auth, getActivityFeed)
